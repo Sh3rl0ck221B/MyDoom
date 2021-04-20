220ac537fbe1e0512d0959786f78dda2da8c2332
Christopher Meyer <christopher_meyer@outlook.de>
2021-04-20 11:16:21 +0200

    Climbing enabled

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,44 +2,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    //Movement
     public float speed = 12f ;
     private Vector3 move;
 
+    //Jump
     public float jumpHeight = 3f;
     public float gravity = -9.81f;
     private Vector3 velocity;
 
+    //DoubleJump
     public Transform feet;
     public LayerMask ground;
-    public float groundDistance = 0.4f;
+    public float distance = 0.4f;
     public bool doubleJump = false;
     public bool isGrounded;
 
+    //Dash
     public float dashTime = 0.25f;
     public float dashSpeed = 20f;
-    public float dashCoolDownTime = 20f;
     private float startCoolDown;
 
+    //Trampoline
     public LayerMask trampoline;
 
+    //Grabbling hook
     public Camera cam;
     public Transform debugHitTransform;
     public Vector3 hookShotPosition;
-    public Vector3 characterVelocityMomentum;
     private float velocityY;
 
-
-
+    //Climb
+    private bool canClimb;
+    public LayerMask climbable;
+    private float speedUpDown = 3.2f;
+    
+    //StateHandling
     private State state;
     private enum State
     {
         Normal,
         HookshotFlyingPlayer,
+        Climbing
     }
 
     void Awake()
@@ -49,6 +59,19 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        canClimb = checkEnvironment(climbable);
+
+        if (canClimb)
+        {
+            state = State.Climbing;
+        }
+        else
+        {
+            controller.enabled = true;
+        }
+        
+        
+        
         switch (state)
         { 
             case State.Normal:
@@ -58,13 +81,37 @@ public class PlayerMovement : MonoBehaviour
             case State.HookshotFlyingPlayer:
                 HandleHookShotMovement();
                 break;
+            case State.Climbing:
+                Climbing();
+                HandleHookshotStart();
+                break;
+        }
+    }
+
+    private void Climbing()
+    {
+        controller.enabled = false;
+        if (Input.GetKey("w"))
+        {
+            transform.position += Vector3.up / speedUpDown;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.down / speedUpDown;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controller.enabled = true;
+            StartCoroutine(Dash(false));
         }
     }
 
     private void HandleCharacterMovement()
     {
-        isGrounded = checkUnderground(ground);
-        
+        isGrounded = checkEnvironment(ground);
+
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
@@ -87,7 +134,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (checkUnderground(trampoline))
+        if (checkEnvironment(trampoline))
         {
             Jump(jumpHeight * 2);
         }
@@ -98,17 +145,24 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(Dash());
+            StartCoroutine(Dash(true));
         }
     }
     
-    IEnumerator Dash()
+    IEnumerator Dash(bool forward)
     {
         float startTime = Time.time;
 
         while (Time.time < startTime + dashTime)
         {
-            controller.Move( transform.forward * dashSpeed * Time.deltaTime);
+            if (forward)
+            {
+                controller.Move( transform.forward * dashSpeed * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move( transform.forward * dashSpeed * Time.deltaTime);
+            }
             yield return null;
         }
     }
@@ -128,9 +182,9 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
     }
 
-    private bool checkUnderground(int ground)
+    private bool checkEnvironment(int ground)
     {
-        return Physics.CheckSphere(feet.position, groundDistance, ground);
+        return Physics.CheckSphere(feet.position, distance, ground);
     }
 
     private void HandleHookshotStart()
@@ -166,6 +220,7 @@ public class PlayerMovement : MonoBehaviour
         }
         
     }
+    
 
 
 
